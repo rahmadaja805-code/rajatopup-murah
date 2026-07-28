1a0fbab306b0126373642a8ef91692cbeb81a486
marketplaceidv5-boop <marketplaceidv5@gmail.com>
2026-07-28 18:59:00 +0700

    Tambah webhook Fonnte

--- routes/fonnte.js
+++ routes/fonnte.js
@@ -2,15 +2,17 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("/webhook", (req, res) => {
+router.get("/webhook", (req, res) => {
+    res.send("Webhook Fonnte Aktif ✅");
+});
 
+router.post("/webhook", (req, res) => {
     console.log("=== WEBHOOK FONNTE ===");
     console.log(req.body);
 
     res.status(200).json({
         success: true
     });
-
 });
 
 export default router;
